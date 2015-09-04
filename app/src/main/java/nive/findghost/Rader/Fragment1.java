7a9af9aa4ee172491808fd5c552c7a49ef8900bc
haninkyu <dlsrb0412@naver.com>
2015-09-04 19:12:59 +0900

    01. NAVER MAP API SETTING

---
+++ app/src/main/java/nive/findghost/Rader/Fragment1.java
@@ -0,0 +1,17 @@
+package nive.findghost.Rader;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nive.findghost.R;
+
+public class Fragment1 extends NMapFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment1, container, false);
+	}
+}
